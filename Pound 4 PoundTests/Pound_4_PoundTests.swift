a8c63aff266680cb00780d08444fb48ce37ea788
Brian Henry <brian.henry2@gmail.com>
2016-01-30 10:33:27 +0000

    Initial Commit

---
+++ Pound 4 PoundTests/Pound_4_PoundTests.swift
@@ -0,0 +1,36 @@
+//
+//  Pound_4_PoundTests.swift
+//  Pound 4 PoundTests
+//
+//  Created by Brian Henry on 30/01/2016.
+//  Copyright © 2016 pound4pound. All rights reserved.
+//
+
+import XCTest
+@testable import Pound_4_Pound
+
+class Pound_4_PoundTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
